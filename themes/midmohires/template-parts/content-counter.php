25f195d3e68362eb8f99c23fefefe602f63a11eb
Jesse Corkill <xxjescorkillxx@gmail.com>
2020-10-20 13:46:08 -0500

    Counters are functional & jobs preview pills are functional

--- themes/midmohires/template-parts/content-counter.php
+++ themes/midmohires/template-parts/content-counter.php
@@ -1,43 +1,42 @@
 <div class="bg-overlay bg-overlay-gradient"></div>
 <div class="container">
     <div class="row" id="counter">
-      
-        <div class="col-md-3 col-6">
+        <div class="text-center col-md-4 col-12">
             <div class="home-counter pt-4 pb-4">
                 <div class="float-left counter-icon mr-3">
                     <i class="mdi mdi-bank h1 text-white"></i>
                 </div>
                 <div class="counter-content overflow-hidden">
-                    <h1 class="counter-value text-white mb-1" data-count="<?php wp_count_terms('company') ?>"></h1>
+                    <h1 class="counter-value text-white mb-1" data-count="<?php Counters::companies() ?>">10</h1>
                     <p class="counter-name text-white text-uppercase mb-0">Companies</p>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-3 col-6">
-            <div class="home-counter pt-4 pb-4">
+        <div class="text-center col-md-4 col-6">
+            <div style="" class="home-counter pt-4 pb-4">
                 <div class="float-left counter-icon mr-3">
                     <i class="mdi mdi-file-document-box h1 text-white"></i>
                 </div>
                 <div class="counter-content overflow-hidden">
-                    <h1 class="counter-value text-white mb-1" data-count="480">80</h1>
+                    <h1 class="counter-value text-white mb-1" data-count="526">80</h1>
                     <p class="counter-name text-white text-uppercase mb-0">Connections</p>
                 </div>
             </div>
         </div>
-      
-        <div class="col-md-3 col-6">
+
+        <div class="text-center col-md-4 col-6">
             <div class="home-counter pt-4 pb-4">
                 <div class="float-left counter-icon mr-3">
                     <i class="mdi mdi-calendar-multiple-check h1 text-white"></i>
                 </div>
                 <div class="counter-content overflow-hidden">
-                    <h1 class="counter-value text-white mb-1" data-count="<?php wp_count_posts("Jobs"); ?>"></h1>
-                    <p class="counter-name text-white text-uppercase mb-0">Jobs Posted</p>
+
+                    <h1 class="counter-value text-white mb-1" data-count="<?php Counters::jobs()?>">4</h1>
+                    <p class="counter-name text-white text-uppercase mb-0"> Job Posted</p>
                 </div>
             </div>
         </div>
-      
     </div>
 
 </div>
