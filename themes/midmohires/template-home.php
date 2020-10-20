66da1726831f7da7fd9c5d2ea6608b95e579193a
Jesse Corkill <xxjescorkillxx@gmail.com>
2020-10-20 09:43:38 -0500

    Incomplete Work on Pagination & Counter &

--- themes/midmohires/template-home.php
+++ themes/midmohires/template-home.php
@@ -514,58 +514,7 @@ get_header(); ?>
 
       <!-- counter start -->
       <section class="section bg-counter position-relative" style="background: url('https://via.placeholder.com/200X700//88929f/5a6270C/O https://placeholder.com/') center center;">
-          <div class="bg-overlay bg-overlay-gradient"></div>
-          <div class="container">
-              <div class="row" id="counter">
-                  <div class="col-md-3 col-6">
-                      <div class="home-counter pt-4 pb-4">
-                          <div class="float-left counter-icon mr-3">
-                              <i class="mdi mdi-bank h1 text-white"></i>
-                          </div>
-                          <div class="counter-content overflow-hidden">
-                              <h1 class="counter-value text-white mb-1" data-count="120">10</h1>
-                              <p class="counter-name text-white text-uppercase mb-0">Companies</p>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="col-md-3 col-6">
-                      <div class="home-counter pt-4 pb-4">
-                          <div class="float-left counter-icon mr-3">
-                              <i class="mdi mdi-file-document-box h1 text-white"></i>
-                          </div>
-                          <div class="counter-content overflow-hidden">
-                              <h1 class="counter-value text-white mb-1" data-count="480">80</h1>
-                              <p class="counter-name text-white text-uppercase mb-0">Applications</p>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="col-md-3 col-6">
-                      <div class="home-counter pt-4 pb-4">
-                          <div class="float-left counter-icon mr-3">
-                              <i class="mdi mdi-calendar-multiple-check h1 text-white"></i>
-                          </div>
-                          <div class="counter-content overflow-hidden">
-                              <h1 class="counter-value text-white mb-1" data-count="120">10</h1>
-                              <p class="counter-name text-white text-uppercase mb-0">Job Posted</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-md-3 col-6">
-                      <div class="home-counter pt-4 pb-4">
-                          <div class="float-left counter-icon mr-3">
-                              <i class="mdi mdi-account-multiple-plus h1 text-white"></i>
-                          </div>
-                          <div class="counter-content overflow-hidden">
-                              <h1 class="counter-value text-white mb-1" data-count="200">10</h1>
-                              <p class="counter-name text-white text-uppercase mb-0">Member</p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-
-          </div>
+        <?php get_template_part("template-parts/content","counter"); ?>
       </section>
       <!-- counter end -->
 
