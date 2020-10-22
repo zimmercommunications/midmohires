96431361fdc9e2931a3d4d0aa0c97a72d540d926
Jesse Corkill <xxjescorkillxx@gmail.com>
2020-10-22 17:07:05 -0500

    Footer Work

--- themes/midmohires/footer.php
+++ themes/midmohires/footer.php
@@ -2,50 +2,23 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
-                <a href="javascript:void(0)"><img src="images/logo-light.png" height="20" alt=""></a>
-                <p class="mt-4">At vero eos et accusamus et iusto odio dignissim os ducimus qui blanditiis praesentium</p>
-                <ul class="social-icon social list-inline mb-0">
-                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="rounded"><i class="mdi mdi-google"></i></a></li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                <p class="text-white mb-4 footer-list-title">Company</p>
-                <ul class="list-unstyled footer-list">
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> About Us</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Media & Press</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Career</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Blog</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Pricing</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Marketing</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> CEOs </a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Agencies</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Our Apps</a></li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                <p class="text-white mb-4 footer-list-title">Resources</p>
-                <ul class="list-unstyled footer-list">
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Support</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Privacy Policy</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Terms</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Accounting </a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> Billing</a></li>
-                    <li><a href="#" class="text-foot"><i class="mdi mdi-chevron-right"></i> F.A.Q.</a></li>
-                </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                <p class="text-white mb-4 footer-list-title f-17">Business Hours</p>
-                <ul class="list-unstyled text-foot mt-4 mb-0">
-                    <li>Monday - Friday : 9:00 to 17:00</li>
-                    <li class="mt-2">Saturday : 10:00 to 15:00</li>
-                    <li class="mt-2">Sunday : Day Off (Holiday)</li>
-                </ul>
-            </div>
+          <!-- Footer Widget 1 -->
+            <style>
+              .centerItem{
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                margin-top: auto;
+                margin-bottom: auto;
+              }
+            </style>
+            <?php dynamic_sidebar('Footer Widget 1'); ?>
+            <!-- Footer Widget 2 -->
+            <?php dynamic_sidebar('Footer Widget 2'); ?>
+            <!-- Footer Widget 3 -->
+            <?php dynamic_sidebar('Footer Widget 3'); ?>
+            <!-- Footer Widget 4 -->
+            <?php dynamic_sidebar('Footer Widget 4'); ?>
         </div>
     </div>
 </footer>
@@ -56,7 +29,7 @@
         <div class="row justify-content-center">
             <div class="col-12">
                 <div class="">
-                    <p class="mb-0">© 2019 -2020 Jobya. Design with <i class="mdi mdi-heart text-danger"></i> by Themesdesign.</p>
+                    <p class="mb-0">© 2020 Mid-Mo Hires. Design by Zimmer Communications.</p>
                 </div>
             </div>
         </div>
