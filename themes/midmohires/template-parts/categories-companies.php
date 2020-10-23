8ef5975f3ba622d34c3dfdd2df7ab5acfdfbb993
Clayton Kreisel <claytonkreisel@gmail.com>
2020-10-23 13:56:15 -0500

    FP Fix

--- themes/midmohires/template-parts/categories-companies.php
+++ themes/midmohires/template-parts/categories-companies.php
@@ -3,7 +3,6 @@
         <div class="col-12">
             <div class="section-title text-center mb-4 pb-2">
                 <h4 class="title title-line pb-5">Companies</h4>
-                <p class="text-muted para-desc mx-auto mb-1">Post a job to tell us about your project. We'll quickly match you with the right freelancers.</p>
             </div>
         </div>
     </div>
